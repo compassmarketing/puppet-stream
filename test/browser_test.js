229655d287061d3130930a810aeff0bfc03563ca
Corey Collins <coreyecollins@gmail.com>
2019-06-10 11:22:46 -0500

    test to check executable path

--- test/browser_test.js
+++ test/browser_test.js
@@ -7,3 +7,13 @@ test('should append extra args to the default args when init puppeteer', async t
   let brower = new Browser({ args: ['--proxy=0.0.0.0'] })
   t.is(brower.defaultArgs().pop(), '--proxy=0.0.0.0')
 })
+
+test('should use custom executablePath', async t => {
+  let browser = new Browser({ executablePath: '/tmp/path' })
+  await t.throwsAsync(
+    async () => {
+      await browser.get()
+    },
+    { message: /^Failed to launch chrome! spawn \/tmp\/path.*/ }
+  )
+})
